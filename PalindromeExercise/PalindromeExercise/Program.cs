42089ee63dbcac18f8b40bdf93636d35484ffc29
TrillOnGit <austinfunk99@yahoo.ca>
2023-07-13 22:58:43 -0400

    Completed Exercise 2 and constructed tests.

--- PalindromeExercise/PalindromeExercise/Program.cs
+++ PalindromeExercise/PalindromeExercise/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Linq;
 
 namespace PalindromeExercise
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            IsPalindrome("ab !a");
+        }
+
+        public static bool IsPalindrome(string palWord)
+        {
+            string palLetters = new string (palWord
+                .Where(ch => char.IsLetter(ch))
+                .Select(x => char.ToLower(x)).ToArray());
+            
+            for (int i = 0; i < palLetters.Length / 2 + 1; i++)
+            {
+                if (palLetters[i] != palLetters[palLetters.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("panama", false)]
+        [InlineData("A man, a plan, a canal - Panama!", true)]
+        public void PalindromeTest(string word, bool expected)
         {
+            //Arrange, Act, Assert
 
+            bool actual = Program.IsPalindrome(word);
+            
+            Assert.Equal(actual, expected);
         }
     }
 }
